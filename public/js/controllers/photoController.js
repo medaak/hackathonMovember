5c1428ba421cb515d03ff30f6fa8e3b315bb0b71
stephsen <stephsen59@gmail.com>
2016-11-09 22:59:53 +0100

    photo

--- public/js/controllers/photoController.js
+++ public/js/controllers/photoController.js
@@ -1,55 +1,20 @@
 class photoController {
 
     constructor() {
-      (function () {
-      var takePicture = document.querySelector("#take-picture"),
-          showPicture = document.querySelector("#show-picture");
+      document.getElementById("take-picture").addEventListener("change", readFile, false);
 
-      if (takePicture && showPicture) {
-          // Set events
-          takePicture.onchange = function (event) {
-              // Get a reference to the taken picture or chosen file
-              var files = event.target.files,
-                  file;
-              if (files && files.length > 0) {
-                  file = files[0];
-                  console.log(file);
-                  try {
-                      // Get window.URL object
-                      var URL = window.URL || window.webkitURL;
-
-                      // Create ObjectURL
-                      var imgURL = URL.createObjectURL(file);
-                      console.log(imgURL);
-                      // Set img src to ObjectURL
-                      showPicture.src = imgURL;
-
-                      // Revoke ObjectURL after imagehas loaded
-                      showPicture.onload = function() {
-                          URL.revokeObjectURL(imgURL);
-                      };
-                  }
-                  catch (e) {
-                      try {
-                          // Fallback if createObjectURL is not supported
-                          var fileReader = new FileReader();
-                          fileReader.onload = function (event) {
-                              showPicture.src = event.target.result;
-                          };
-                          fileReader.readAsDataURL(file);
-                      }
-                      catch (e) {
-                          // Display error message
-                          var error = document.querySelector("#error");
-                          if (error) {
-                              error.innerHTML = "Neither createObjectURL or FileReader are supported";
-                          }
-                      }
-                  }
-              }
+      function readFile() {
+        if (this.files && this.files[0]) {
+          var FR= new FileReader();
+          FR.onload = function(e) {
+            document.getElementById("show-picture").src = e.target.result;
+            document.getElementById("b64").innerHTML = e.target.result;
+            console.log(e.target.result);
+            console.log(atob(e.target.result));
           };
+          FR.readAsDataURL( this.files[0] );
+        }
       }
-  })();
     }
 
 }
