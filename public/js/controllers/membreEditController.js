c32aba1c82053f4bba3f88de66635d6810ab16bc
medaak <medaakdev@gmail.com>
2016-11-09 21:59:30 +0100

    oops

--- public/js/controllers/membreEditController.js
+++ public/js/controllers/membreEditController.js
@@ -8,10 +8,16 @@ class membreEditController {
     load() {
       this.memberID="58237d189713dc551eaa986a"
       console.log(this.memberID);
-      
+
         this.membreService.getOne(this.memberID).then((res) => {
 
             this.membre = res.data;
+        })
+    }
+
+    update(membre) {
+        this.membreService.update(membre._id, membre.pseudoMembre, membre.nomMembre, membre.prenomMembre, membre.photoMoustacheMembre, membre.noteMoustacheMembre, membre.dateNaissanceMembre, membre.villeMembre, membre.adresseMailMembre, membre.moustacheJour).then(() => {
+            this.load()
         })
     }
 
