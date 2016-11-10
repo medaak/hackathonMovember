a65475014478a2697c3368735ff24a470fb8224f
stephsen <stephsen59@gmail.com>
2016-11-10 03:35:02 +0100

    fix erreur

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -3,25 +3,13 @@ class mainController {
     constructor(todoService) {
         this.todoService = todoService;
         this.load();
+
         $('.carousel').carousel({
             interval: 5000 //changes the speed
-        })
-
-        /*#configuration-variables*/
-/*
-var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-*/
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = '//movembermoustache.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
+        });
 
     }
+
 
     load() {
         this.todoService.getAll().then((res) => {
